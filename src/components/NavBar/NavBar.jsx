bf09d3e6d13365e445338da6921f3f134df36c36
Emilio Filandini <emiliofilandini@gmail.com>
2024-04-02 16:41:07 -0300

    agrego NavBar.jsx a App

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>Ecomerce</h3>
+      <div>
+        <button>Celulares</button>
+        <button>Tablets</button>
+        <button>Notebooks</button>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
