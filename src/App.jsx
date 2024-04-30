e476fceacf94679266958fd52e1b0f76f6dddaa5
Emilio <emiliofilandini@gmail.com>
2024-04-30 17:27:34 -0300

    agrego router

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListcontainer/ItemListcontainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting="Bienvenidos" />
-      <ItemDetailContainer></ItemDetailContainer>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemListContainer />} />
+          <Route path="*" element={<h4>Elemento No Encontrado</h4>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
