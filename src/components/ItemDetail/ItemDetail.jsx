e39da0a36ba28a7ccb26164c86dff8880f036478
Emilio Filandini <emiliofilandini@gmail.com>
2024-05-22 19:32:38 -0300

    se agrega handleOnAdd y useEffect para llevar la cantidad agregada

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 
 const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
+  const [quantityAdded, setQuantityAdded] = useState(0);
+  const handleOnAdd = (quantity) => {
+    setQuantityAdded(quantity);
+  };
+
   return (
     <article className="CardItem">
       <header className="Header">
@@ -16,11 +23,17 @@ const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
         <p className="Info">Stock: {stock}</p>
       </section>
       <footer className="ItemFooter">
-        <ItemCount
-          initial={0}
-          stock={stock}
-          onAdd={(quantity) => console.log("Cantidad Agregada", quantity)}
-        />
+        {quantityAdded > 0 ? (
+          <Link to="/cart" className="Button">
+            Terminar Compra
+          </Link>
+        ) : (
+          <ItemCount
+            initial={0}
+            stock={stock}
+            onAdd={handleOnAdd}
+          />
+        )}
       </footer>
     </article>
   );
